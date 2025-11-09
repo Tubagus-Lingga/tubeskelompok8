df73a8a375c14ee10c97fccd00c3425e1d4f79bc
fauzidho <fauzibae288@gmail.com>
2025-11-09 23:07:54 +0700

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User; // Pastikan mengimpor Model User
+use App\Models\User;
 use Illuminate\Validation\Rules;
 
 class RegisteredUserController extends Controller
