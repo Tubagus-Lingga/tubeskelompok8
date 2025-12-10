82c647dfb006ff98978503cfa04e8f75a04d7124
Tubagus Lingga <email_github_tubaguslinggaap@gmail.com>
2025-12-10 12:03:11 +0700

    fix: ensure Product model autoloads

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,28 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
-
-    /**
-     * Nama tabel (opsional jika tabel = 'products')
-     */
-    protected $table = 'products';
-
-    /**
-     * Field yang boleh diisi mass-assignment
-     */
-    protected $fillable = [
-        'name',
-        'slug',
-        'price',
-        'description',
-        'stock',
-        'category_id',
-        'image',
-    ];
+    protected $guarded = [];
 }
